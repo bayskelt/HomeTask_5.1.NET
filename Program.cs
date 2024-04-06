3219ea2e29e768d6912a3b8ab3b080bd8f49a553
bayskelt <rinognom@gmail.com>
2024-04-06 22:43:41 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System.Runtime.InteropServices;
+
+namespace HomeTask_5._1.NET
+{
+    internal class Program
+    {
+        static void Avarage(decimal n1, decimal n2)
+        {
+            decimal avarage = (n1 + n2) / 2;
+            Console.Write($"Середнє арифметичне цих чисел: {avarage}");
+        }
+        static void Main()
+        {
+            Console.Write("Введiть перше цiле число: ");
+            int x = int.Parse( Console.ReadLine() );
+            Console.Write("Введiть друге цiле число: ");
+            int y = int.Parse( Console.ReadLine() );
+
+            Avarage( x, y );
+        }
+    }
+}
